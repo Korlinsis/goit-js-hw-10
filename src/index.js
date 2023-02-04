492e0679a56cc84e852b722251918f8216088059
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-02-04 19:27:40 +0200

    Add libraries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from "lodash.debounce";
+import { fetchCountries } from "./fetchCountries";
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.getElementById('search-box');
+const list = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+
+inputEl.addEventListener('input', debounce(getName, DEBOUNCE_DELAY));
+
+function getName(e) {
+    fetchCountries(e.target.value);
+    Notify.success(`${e.target.value} Слава Україні!`);
+}
+
